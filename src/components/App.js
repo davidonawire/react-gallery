c2345f7598b3af9776fc4c626338a49fee948e93
David Adams <onethinline@gmail.com>
2020-08-19 08:49:59 -0700

    Clean up src structure

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './Nav';
+import Gallery from './Gallery';
+
+// Search
+// Nav
+// PhotoContainer
+// Photo
+
+function App() {
+  return (
+    <div className="container">
+      <Nav />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Gallery = () => (
+  <div className="photo-container">
+
+  </div>
+);
+
+export default Gallery;
